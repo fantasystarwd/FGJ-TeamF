4f834f04403a87fc771879abea6542ee63535fd6
vvbest2012 <vvbest2012@gmail.com>
2020-08-29 19:33:15 +0800

    老鼠打針流程

---
+++ Assets/william/scripts/MouseManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MouseManager : MonoBehaviour
+{
+    public Animator mouseAnimator;
+    public Text mouseTag;
+    public GameObject Syringe;
+
+    private bool canOut = false;
+    private bool canInjection = false;
+    private int mouseTagValue = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CallMouseIn()
+    {
+        if (!canOut)
+        {
+            Syringe.SetActive(false);
+            mouseAnimator.SetTrigger("MouseIn");
+            mouseTag.text = mouseTagValue.ToString();
+            canOut = true;
+            canInjection = true;
+        }
+    }
+
+    public void CallMouseIOut()
+    {
+        if (canOut && !canInjection)
+        {
+            Syringe.SetActive(false);
+            mouseAnimator.SetTrigger("MouseOut");
+            canOut = false;
+            mouseTagValue++;
+        } 
+    }
+
+    public void CallSyringeInjection()
+    {
+        if (canInjection)
+        {
+            //mouseAnimator.SetTrigger("ResetSyringe");
+            Syringe.SetActive(true);
+            mouseAnimator.SetTrigger("SyringeInjection");
+            canInjection = false;
+        }
+    }
+}
