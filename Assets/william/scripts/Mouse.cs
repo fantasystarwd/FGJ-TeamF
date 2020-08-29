5b0e76df29619b57461c52b146f6557c480be18f
vvbest2012 <vvbest2012@gmail.com>
2020-08-29 21:33:59 +0800

    老鼠打針判斷

---
+++ Assets/william/scripts/Mouse.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse : MonoBehaviour
+{
+    public MouseManager mouseManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Check()
+    {
+        Debug.Log("打針結束");
+        mouseManager.CheckResult();
+    }
+}
