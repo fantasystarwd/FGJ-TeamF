f4dc10782b64b450628c3b19dafc9288ea2f7ef0
vvbest2012 <vvbest2012@gmail.com>
2020-08-30 15:45:34 +0800

    update

--- Assets/Scripts/GlobalParameter.cs
+++ Assets/Scripts/GlobalParameter.cs
@@ -4,6 +4,9 @@
     public static bool stageResult_2 = false;
     public static bool stageResult_3 = false;
 
+    public static bool goOtherCompany = false;
+    public static bool stageResult_2_fail = false;
+
     public static void SuccessCheckPoint()
     {
 
