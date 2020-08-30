d7b299520c726ddc42fca66bf99e240a09ea11a0
chen hung-chan <fantasystar951@gmail.com>
2020-08-30 13:26:58 +0800

    add checkPoint2 needed Image

--- Assets/Scripts/CheckPoint2Manager.cs
+++ Assets/Scripts/CheckPoint2Manager.cs
@@ -1,77 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 using UnityEngine.UI;
+
 
 public class CheckPoint2Manager : MonoBehaviour
 {
-    public Text mentionText;
-    public Image resultImage;
-    public Button buttonYes, buttonNo;
-
-    public string[] mentionTextList;
-    public bool[] mentionTextResult;
-    public int[] mentionTextPoint;
-
-    public int resultBestPoint = 80;
-
-    public Sprite[] resultImageChoose = new Sprite[2]; 
-
-    private int mentionTextCounter = -1;
-    private int checkPoint = 0;
-
-    public void activateButton(bool isActivate)
+    [SerializeField]
+    public enum TestTubeColor
     {
-        buttonYes.gameObject.SetActive(isActivate);
-        buttonNo.gameObject.SetActive(isActivate);
+        Red = 0,
+        Yellow,
+        Green,
+        Blue
     }
 
-    public bool updateMentionText()
+    private List<Color> addedColorList = new List<Color>();
+
+    public Image petrl_water;   
+
+    public void Middle_End()
     {
-        mentionTextCounter++;
-        if(mentionTextCounter >= mentionTextList.Length)
-        {
-            return false;
-        }
-        else
-        {
-            showTextAnim(mentionTextCounter);
-            return true;
-        }
+        Debug.Log("Moddle End");
     }
 
-    public void click_check_button(bool result)
+    public void Add_New_Color_In_Petrl(TestTubeColor tubeColor)
     {
-        if(result == mentionTextResult[mentionTextCounter])
+        Debug.Log(tubeColor);
+        switch (tubeColor)
         {
-            checkPoint += mentionTextPoint[mentionTextCounter];
-        }
-        else
-        {
+            case TestTubeColor.Red:
+                addedColorList.Add(Color.red);
+                break;
+            case TestTubeColor.Yellow:
+                addedColorList.Add(Color.yellow);
+                break;
+            case TestTubeColor.Green:
+                addedColorList.Add(Color.green);
+                break;
+            case TestTubeColor.Blue:
+                addedColorList.Add(Color.blue);
+                break;
 
         }
-    }
 
-    public void Show_Result()
-    {
-        if(checkPoint >= resultBestPoint)
+        Color targetColor = Color.black;
+        foreach (Color item in addedColorList)
         {
-            showResultAnim(0);
+            targetColor += item;
         }
-        else
-        {
-            showResultAnim(1);
-        }
-    }
+        targetColor.a = 0.5f;
 
-    private void showTextAnim(int counter)
-    {
-        mentionText.text = mentionTextList[mentionTextCounter];
+        Tween tween = DOTween.To(() => petrl_water.color, r => petrl_water.color = r, targetColor, 1.5f).SetEase(Ease.OutCubic);
     }
-
-    private void showResultAnim(int result)
-    {
-        resultImage.sprite = resultImageChoose[result];
-    }
-    
 }
