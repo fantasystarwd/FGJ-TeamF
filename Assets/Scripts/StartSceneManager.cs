e042ff23c9b32ae3d0c7a1c5fbdf4de0ff0c73b6
chen hung-chan <fantasystar951@gmail.com>
2020-08-30 15:20:34 +0800

    Add start scene

---
+++ Assets/Scripts/StartSceneManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class StartSceneManager : MonoBehaviour
+{
+    public Image startText;
+    public float fadeTime = 1.5f;
+    Tween tween;
+    public void playStartSceneAnim()
+    {
+        FadeOut();
+    }
+
+    public void Stop()
+    {
+        tween.Kill();
+    }
+
+    private void FadeOut()
+    {
+        tween = DOTween.To(() => startText.color, r => startText.color = r, new Color(1,1,1,0), fadeTime).SetEase(Ease.OutCubic);
+        tween.onComplete += FadeIn;
+    }
+
+    private void FadeIn()
+    {
+        tween = DOTween.To(() => startText.color, r => startText.color = r, new Color(1, 1, 1, 1), fadeTime).SetEase(Ease.InCubic);
+        tween.onComplete += FadeOut;
+    }
+}
