cf72372543ed1f3072b677cf5ffdff7baab9bce7
vvbest2012 <vvbest2012@gmail.com>
2020-08-30 15:35:06 +0800

    end scene

--- Assets/william/scripts/EndGame.cs
+++ Assets/william/scripts/EndGame.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class EndGame : MonoBehaviour
 {
+    public Fungus.Flowchart flowchart;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void check()
+    {
+        if (!GlobalParameter.stageResult_1 && GlobalParameter.stageResult_2 && !GlobalParameter.stageResult_3)
+        {
+            Debug.Log("結局A");
+            flowchart.ExecuteBlock("A");
+        }
+        else if (!GlobalParameter.stageResult_1 && !GlobalParameter.stageResult_2 && !GlobalParameter.stageResult_3)
+        {
+            Debug.Log("結局C_1");
+            flowchart.ExecuteBlock("C_1");
+        }
+        else
+        {
+            Debug.Log("結局C_2");
+            flowchart.ExecuteBlock("C_2");
+        }
     }
 }
